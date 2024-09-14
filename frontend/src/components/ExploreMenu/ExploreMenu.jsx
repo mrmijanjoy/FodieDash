40dce13eaf20278811c3c6a603fde7f534329267
Mijanur Rahman Joy <mrmijanjoy@gmail.com>
2024-09-14 08:52:13 +0600

    Header completed

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './ExploreMenu.css'
+
+const ExploreMenu = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExploreMenu
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favorite foods here</h2>
+        <p>text ever since the 1500s, when an unknown printer took a text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
